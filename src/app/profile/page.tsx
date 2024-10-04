9e3558e1ee4ff54f353db0d2411ef3561f9ea28c
auraleaas <116437862+auraleaas@users.noreply.github.com>
2024-10-04 13:03:19 +0700

    feat: add profile, category, card

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,38 +1,23 @@
 // pages/profile.js
 import { api } from "@/trpc/react";
 import { getServerAuthSession } from "@/server/auth";
-import {ProfilePage} from "@/app/profile/profile";
+import { ProfileContent, ProfilePage } from "@/app/profile/profile";
 
-const Profile = async() => {
 
-    const session =  await getServerAuthSession()
-    const userId = session?.user.id
+const Profile = async () => {
+    const session = await getServerAuthSession();
+    const userId = session?.user.id;
     if (!userId) {
-        return null
-    }
-    const getUser = api.user.getUser.useQuery({id: userId})
-
-    if (getUser.isLoading){
-        return <div>Loading...</div>
-    }
-
-    const user = getUser.data
-    if (!user){
-        return <div>User not found</div>
-    }
-    const {name, email, phoneNumber, password} = user
-    const formattedUser = {
-        name,
-        email: email?? "",
-        telp: phoneNumber,
-        password
+        return null;
     }
 
     return (
         <main>
-            <ProfilePage user={formattedUser}/>
+            <ProfileContent userId={userId} />
         </main>
-    )
+    );
 };
+
+
 
 export default Profile;
